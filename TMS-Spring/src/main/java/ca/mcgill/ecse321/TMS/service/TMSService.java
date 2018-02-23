dd6700c883ce13091402088a7ce79b7e9e0680aa
Fouad-El-Bitar <fouad.elbitar@mail.mcgill.ca>
2018-02-23 17:52:11 -0500

    Trying to fix bug

--- TMS-Spring/src/main/java/ca/mcgill/ecse321/TMS/service/TMSService.java
+++ TMS-Spring/src/main/java/ca/mcgill/ecse321/TMS/service/TMSService.java
@@ -1,19 +1,28 @@
 package ca.mcgill.ecse321.TMS.service;
 
 import java.util.List;
+import java.sql.Date;
+import java.util.Calendar;
 
 import org.springframework.stereotype.Service;
 
-import ca.mcgill.ecse321.TMS.dto.TreeDto;
+import ca.mcgill.ecse321.TMS.model.LocationType;
 import ca.mcgill.ecse321.TMS.model.Municipality;
+import ca.mcgill.ecse321.TMS.model.Park;
 import ca.mcgill.ecse321.TMS.model.Species;
+import ca.mcgill.ecse321.TMS.model.Street;
 import ca.mcgill.ecse321.TMS.model.Tree;
+import ca.mcgill.ecse321.TMS.model.TreeLocation;
 import ca.mcgill.ecse321.TMS.model.TreePLE;
+import ca.mcgill.ecse321.TMS.model.TreeStatus;
+import ca.mcgill.ecse321.TMS.model.User;
+
+
 
 @Service
 public class TMSService {
 
-	private TreePLE treePle;
+	
 	
 
 	public List<Tree> getTreesForMunicipality(Municipality m) {
