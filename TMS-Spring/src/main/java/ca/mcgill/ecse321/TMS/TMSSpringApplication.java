5165bd4e7ea350872dc222b2db14f1f391a6f24f
Imad <imad.dodin@mail.mcgill.ca>
2018-03-29 14:10:22 -0400

    Configure Server Ports
    Further implement server properties
    
    Edit CORS Mappings
    
    Further edit CORS Mappings
    
    Remove CORS Mapping
    
    Attempt to allow requests from all origins
    
    Further update CORS Settings
    
    Fix bug in Spring Application

--- TMS-Spring/src/main/java/ca/mcgill/ecse321/TMS/TMSSpringApplication.java
+++ TMS-Spring/src/main/java/ca/mcgill/ecse321/TMS/TMSSpringApplication.java
@@ -59,9 +59,8 @@ public class TMSSpringApplication extends SpringBootServletInitializer {
 				//Allow android client
 				String androidUrl = "http://" + androidProperties.getIp() + ":" + androidProperties.getPort();
 				//For debug purposes, allow connecting  from localhost as well
-				registry.addMapping("/**").allowedOrigins(frontendUrl, androidUrl, "http://localhost:8087", "http://127.0.0.1:8087");
+				registry.addMapping("/**");
 			}
 		};
 	}
-
 }
