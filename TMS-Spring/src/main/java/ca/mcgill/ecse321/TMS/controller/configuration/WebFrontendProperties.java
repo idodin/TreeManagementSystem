403e55d11eecf98ab122aa7b74b722d063738632
Imad <imad.dodin@mail.mcgill.ca>
2018-03-30 17:00:33 -0400

    Revert "Update Web Properties"
    This reverts commit b68bcb28476a6f31bc9b23301e071747a5240540.

--- TMS-Spring/src/main/java/ca/mcgill/ecse321/TMS/controller/configuration/WebFrontendProperties.java
+++ TMS-Spring/src/main/java/ca/mcgill/ecse321/TMS/controller/configuration/WebFrontendProperties.java
@@ -10,7 +10,7 @@ public class WebFrontendProperties {
 	/**
 	 * The IP adress of the web frontend client
 	 */
-	private String ip = "132.206.44.21";
+	private String ip = "127.0.0.1";
 	/**
 	 * The port on which the web frontend listens
 	 */
