2fd86aff71eedef8b4f1c37ae0b1263f34f4518a
Imad <imad.dodin@mail.mcgill.ca>
2018-03-30 17:11:48 -0400

    Revert "Configure Server Ports"
    This reverts commit 5165bd4e7ea350872dc222b2db14f1f391a6f24f.

--- TMS-Spring/src/main/java/ca/mcgill/ecse321/TMS/TMSSpringApplication.java
+++ TMS-Spring/src/main/java/ca/mcgill/ecse321/TMS/TMSSpringApplication.java
@@ -59,8 +59,9 @@ public class TMSSpringApplication extends SpringBootServletInitializer {
 				//Allow android client
 				String androidUrl = "http://" + androidProperties.getIp() + ":" + androidProperties.getPort();
 				//For debug purposes, allow connecting  from localhost as well
-				registry.addMapping("/**");
+				registry.addMapping("/**").allowedOrigins(frontendUrl, androidUrl, "http://localhost:8087", "http://127.0.0.1:8087");
 			}
 		};
 	}
+
 }
