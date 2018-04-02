b9241a9da0b16749e7f881059de2be3889e5c8e9
Imad <imad.dodin@mail.mcgill.ca>
2018-04-02 12:40:40 -0400

    Generate initial Android Content

--- TMS-Android/app/src/main/java/ca/mcgill/ecse321/tms/MainActivity.java
+++ TMS-Android/app/src/main/java/ca/mcgill/ecse321/tms/MainActivity.java
@@ -8,8 +8,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.entity.mime.Header;
 
 public class MainActivity extends AppCompatActivity {
+    private String error = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +36,39 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        refreshErrorMessage();
+    }
+
+    public void addParticipant(View v) {
+        error = "";
+        final TextView tv = (TextView) findViewById(R.id.newparticipant_name);
+        HttpUtils.post("participants/" + tv.getText().toString(), new RequestParams(), new JsonHttpResponseHandler() {
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                refreshErrorMessage();
+                tv.setText("");
+            }
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                try {
+                    error += errorResponse.get("message").toString();
+                } catch (JSONException e) {
+                    error += e.getMessage();
+                }
+                refreshErrorMessage();
+            }
+        });
+    }
+
+    private void refreshErrorMessage() {
+        // set the error message
+        TextView tvError = (TextView)findViewById(R.id.error);
+        tvError.setText(error);
+
+        if (error == null || error.length() == 0) {
+            tvError.setVisibility(View.GONE);
+        } else {
+            tvError.setVisibility(View.VISIBLE);
+        }
+
     }
 
     @Override
@@ -48,5 +91,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
+    }3
 }
