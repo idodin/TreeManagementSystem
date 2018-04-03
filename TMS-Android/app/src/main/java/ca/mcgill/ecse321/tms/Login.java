15822abedaf333f88df49c454201ce870edcf988
Imad <imad.dodin@mail.mcgill.ca>
2018-04-03 00:08:02 -0400

    Implement Android Login and Remove Title Bars

---
+++ TMS-Android/app/src/main/java/ca/mcgill/ecse321/tms/Login.java
@@ -0,0 +1,18 @@
+package ca.mcgill.ecse321.tms;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+
+public class Login extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_login);
+    }
+}
