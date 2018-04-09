6bdcb2dd07f5c0dcc642e21ea8c8030768456751
Nada Marawan <nada.marwan@mail.mcgill.ca>
2018-04-08 21:20:15 -0400

    Change map marker icon - Add Green tree

--- TMS-Android/app/src/main/java/ca/mcgill/ecse321/tms/MainActivity.java
+++ TMS-Android/app/src/main/java/ca/mcgill/ecse321/tms/MainActivity.java
@@ -24,6 +24,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.loopj.android.http.JsonHttpResponseHandler;
@@ -121,7 +122,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(final GoogleMap googleMap){
         LatLng sydney = new LatLng(-33.852, 151.211);
         googleMap.addMarker(new MarkerOptions().position(sydney)
-                .title("Marker in Sydney"));
+                .title("Marker in Sydney").icon(BitmapDescriptorFactory.fromResource(R.drawable.treeicon)));
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
         googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
