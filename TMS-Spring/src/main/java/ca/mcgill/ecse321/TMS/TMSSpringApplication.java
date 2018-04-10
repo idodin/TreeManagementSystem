6fc7a78211bf66eed0e186080bcaabd19cad192d
ahmed.elehwany <ahmed.elehwany@mail.mcgill.ca>
2018-04-10 17:51:51 -0400

    Update TMSSpringApplication.java

--- TMS-Spring/src/main/java/ca/mcgill/ecse321/TMS/TMSSpringApplication.java
+++ TMS-Spring/src/main/java/ca/mcgill/ecse321/TMS/TMSSpringApplication.java
@@ -59,7 +59,7 @@ public class TMSSpringApplication extends SpringBootServletInitializer {
 				//Allow android client
 				String androidUrl = "http://" + androidProperties.getIp() + ":" + androidProperties.getPort();
 				//For debug purposes, allow connecting  from localhost as well
-				registry.addMapping("/**").allowedOrigins(frontendUrl, androidUrl, "http://localhost:8087", "http://127.0.0.1:8087");
+				registry.addMapping("/**").allowedOrigins(frontendUrl, androidUrl, "http://ecse321-9.ece.mcgill.ca:8087", "http://ecse321-9.ece.mcgill.ca:8087");
 			}
 		};
 	}
