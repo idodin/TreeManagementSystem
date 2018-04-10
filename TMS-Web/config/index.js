0f1c9c45bc726cb9d6f55eca686456bee84a1511
ahmed.elehwany <ahmed.elehwany@mail.mcgill.ca>
2018-04-10 18:57:38 -0400

    Update index.js

--- TMS-Web/config/index.js
+++ TMS-Web/config/index.js
@@ -28,7 +28,7 @@ module.exports = {
   dev: {
     env: require('./dev.env'),
     port: 8087,
-    host:  'localhost',
+    host:  'ecse321-9.ece.mcgill.ca',
     backendHost: 'ecse321-9.ece.mcgill.ca', // set it to real address of backend VM
     backendPort: 8080,
     autoOpenBrowser: true,
